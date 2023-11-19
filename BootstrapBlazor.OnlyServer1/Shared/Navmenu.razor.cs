6f7d1f7c23c178363f85f6882ccf2131c78f68c4
NoahCole <colenc1@etsu.edu>
2023-11-19 18:09:34 -0500

    Add project files.

---
+++ BootstrapBlazor.OnlyServer1/Shared/Navmenu.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BootstrapBlazor.OnlyServer1.Shared
+{
+    public partial class Navmenu
+    {
+        public Navmenu()
+        {
+
+        }
+    }
+}
